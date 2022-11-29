1d79340bef9a0ae7315381ce128258e24babd4be
styagi <styagi@avaya.com>
2022-11-29 18:57:02 +0530

    tree inverted

---
+++ src/main/java/com/saurabh/ds/tree/BinaryTreeInversion.java
@@ -0,0 +1,34 @@
+package com.saurabh.ds.tree;
+
+public class BinaryTreeInversion {
+
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+        	return null;
+        if(root.left !=null && root.right!=null) {
+        	TreeNode left = root.left;
+        	root.left = root.right;
+        	root.right = left;
+        	invertTree(root.left);
+        	invertTree(root.right);
+        } else if(root.left !=null) {
+        	root.right = root.left;
+        	root.left = null;
+        	invertTree(root.right);
+        } else if(root.right !=null) {
+        	root.left = root.right;
+        	root.right = null;
+        	invertTree(root.left);
+        }
+        	
+        return root;
+    }
+    
+    public static void main(String[] args) {
+		BinaryTreeInversion bti = new BinaryTreeInversion();
+		
+		TreeNode root = new TreeNode(100);
+		
+		bti.invertTree(root);
+	}
+}
